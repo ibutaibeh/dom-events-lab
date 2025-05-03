a543065f13f3eed556a70b209165b5ebb217eb51
ibutaibeh <ibutaibeh@hotmail.com>
2025-05-03 13:58:53 +0300

    HW done

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
-//const calculator = document.querySelector('#calculator');
-const buttonNumber = document.getElementsByClassName('button number');
-const buttonOperator = document.getElementsByClassName('button operator');
-const calDisplay= document.querySelector('.display')
-const buttonResult=document.getElementsByClassName('button equals');
+
 /*-------------------------------- Variables --------------------------------*/
 let num1;
 let num2;
 let calculatorOperator;
 let result;
 /*------------------------ Cached Element References ------------------------*/
-
+const buttonNumber = document.getElementsByClassName('button number');
+const buttonOperator = document.getElementsByClassName('button operator');
+const calDisplay= document.querySelector('.display')
+const buttonResult=document.getElementsByClassName('button equals');
 /*----------------------------- Event Listeners -----------------------------*/
-
-//helper code test; since buttonNumber has more than one item (button) i should use for loop function to listen for events.
+//listen for number button
 for (let i = 0; i < buttonNumber.length; i++) {
   buttonNumber[i].addEventListener('click', (event) => {
 
@@ -27,11 +25,18 @@ for (let i = 0; i < buttonNumber.length; i++) {
 
   });
  }
+
+ //listen for opreator button
 for(let j=0; j<buttonOperator.length;j++){
     buttonOperator[j].addEventListener('click',(event)=>{
 
         if(event.target.innerText==='C'){
             calDisplay.textContent='';
+            num1='';
+            num2='';
+            calculatorOperator='';
+            result=0
+        
         }else{
             calDisplay.textContent='';
             calculatorOperator=event.target.innerText;
@@ -39,53 +44,24 @@ for(let j=0; j<buttonOperator.length;j++){
         })
 }
 
-for(let k=0; k<buttonResult.length;k++){
-buttonResult[k].addEventListener('click',(event)=>{
-    console.log(event.target.innerText);
-    calDisplay.textContent= resultFucntion(num1,num2,calculatorOperator);
-    //calDisplay.textContent=event.target.innerText;
-})
+//listen for result button
+if(calculatorOperator!==''){
+    for(let k=0; k<buttonResult.length;k++){
+        buttonResult[k].addEventListener('click',(event)=>{
+            console.log(event.target.innerText);
+            calDisplay.textContent= resultFucntion(num1,num2,calculatorOperator);
+        })
+        }
+}else{
+    calDisplay.textContent=''
 }
 
 
-/*-------------------------------- Functions --------------------------------*/
-//Calculator Fuctions
-// //Addition
-// const additionFucntion=(num1,num2)=>{
-// return num1+num2;
-// }
-// //Substraction
-// const substractionFucntion=(num1,num2)=>{
-//     return num1-num2;
-//     }
-// //Multiplication
-// const multiplicationFucntion=(num1,num2)=>{
-//     return num1*num2;
-//     }
-// //Division
-// const divisionFucntion=(num1,num2)=>{
-//     return num1/num2;
-//     }
-// //Clear Calculator
-// const clearFucntion=()=>{
-//     console.log('should clear the calculator');
-    
-//     }
-//Result Calculator
-const resultFucntion=(x,y,z)=>{
-    if(z==='+'){
-result=x+y;
-return result;
-    }else if(z==='*'){
-result=x*y;
-return result;
-    }else if(z==='-'){
-result=x-y;
-return result;
-    }else{
-result=x/y;
-return result;
-    }
-    console.log(result);
 
+/*-------------------------------- Functions --------------------------------*/
+
+const resultFucntion=(x,y,z)=>{
+result=eval(`${x}${z}${y}`);
+    num1=result;
+return result;
     }
